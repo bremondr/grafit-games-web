d37c5bee923ee5ecf4b4b80a5cb6d53ea2a3b3b3
Ondra <ardnobrem@gmail.com>
2025-11-30 15:17:19 +0100

    increasing snow

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -204,7 +204,7 @@ function registerEvents() {
 
 function initSnow() {
   const holder = document.getElementById("snow");
-  const count = 60;
+  const count = 60 * (currentDay || 1);
   for (let i = 0; i < count; i++) {
     const flake = document.createElement("i");
     flake.className = "flake";
